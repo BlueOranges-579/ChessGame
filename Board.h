9ca2c770e3cd5ff373371fbc9a19d55c213586b3
Luis <luislopez17268@gmail.com>
2025-07-10 10:15:34 -0700

    Initial commit

---
+++ Board.h
@@ -0,0 +1,42 @@
+//
+// Created by blueoranges on 7/10/25.
+//
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <string>
+
+
+    class Board {
+    private:
+        // State for piece that is active
+        enum Piece { N, K, Q, P, B, R };
+
+        // Track whose turn it is
+        enum Player { WHITE, BLACK };
+
+        char board[8][8] = {
+            'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r',
+            'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+            'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
+            'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R',
+        };
+
+        bool playerTurn = false;
+
+    public:
+        void printBoard();
+
+        bool checkWin();
+
+        void checkMove(std::string, std::string);
+};
+
+
+
+#endif //BOARD_H
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Board.h"
+
+int main() {
+    std::string input;
+
+    // Create a Board object
+    Board board = Board();
+
+    // Print the board onto the screen
+    board.printBoard();
+
+    // Enter a move
+    std::cout << "Enter a move: " << std::endl;
+    std::cin >> input;
+
+    board.checkMove(input, input);
+    return 0;
+}
